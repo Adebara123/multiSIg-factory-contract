cb63bc331ca3455b3638a7ac10ad2e52273e6608
Adebara Ayomide <adebaraayomide.com>
2022-08-16 14:38:30 +0100

    addition

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -6,18 +6,12 @@ import { ethers } from "hardhat";
 //import IMultiSig from "../contracts/Imultisig.sol";
 
 async function main() {
-  let provider = {
-    PrivateKey: process.env.PRIVATE_KEY as BytesLike,
-    URL: process.env.ROPSTEN_URL,
-  };
-  const provider2 = ethers.getDefaultProvider("ropsten", provider.URL);
-  let wallet = new ethers.Wallet(provider.PrivateKey, provider2);
+
   const _value = ethers.utils.parseEther("1");
 
   const CONTRACTADDRESS = "0x6e828b59fc799b6ef92e42d2f39e438a7477f469";
   const MULTISIG = await ethers.getContractAt("Imultisig", CONTRACTADDRESS);
-  //   await wallet.sendTransaction({ to: CONTRACTADDRESS, value: _value });
-  //   console.log();
+
   //   console.log("contractBalanc", await MULTISIG.contractBalance());
   //console.log("")
   await MULTISIG.withdrawEther(_value);
